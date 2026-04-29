1badebc025fd24ea55de9e5ba4898f97d5781050
harsshittabhati <harshubhati51@gmail.com>
2026-04-29 23:52:38 +0530

    Initializing project setup

---
+++ src/main/java/com/mindcompanion/MindCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.mindcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindCompanionApplication.class, args);
+	}
+
+}
